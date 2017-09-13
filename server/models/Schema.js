9127741ff735ea3b8d9bcd5a50817b040eaa61a9
Aamel Unia <aameluni@buffalo.edu>
2017-09-12 21:37:39 -0400

    DB Schema updated but still missing points 5 and 6 from Semptember 11 meeting minutes

--- server/models/Schema.js
+++ server/models/Schema.js
@@ -1,25 +1,21 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
-require('mongoose-type-email');
-
 
 const userSchema = new Schema({
 
     first_name:                 { type: String, required: true },
     last_name:                  { type: String, required: true },
-    email:                      { type: mongoose.SchemaTypes.Email, required: true, unique: true },
+    email:                      { type: String, required: true, unique: true },
     person_number:              { type: String, unique: true },
-    major:                      { type: String },
-    graduation_year:            { type: String },
-    admin:                      { type: Boolean },
-    last_ip:                    { type: String },
+    sys_role:                   { type: String },
     created_at:                 { type: Date, default: Date.now, required: true },
     updated_at:                 { type: Date },
+
     courses:                    [{
-                                        _id:                false,
-                                        course_id:          { type: Schema.Types.ObjectId, ref: 'Courses', required: true },
-                                        instructor:         { type: Boolean },
-                                        section_id:         { type: Schema.Types.ObjectId, ref: 'Courses.sections' },
+                                    _id:                false,
+                                    course_id:          { type: Schema.Types.ObjectId, ref: 'Courses', required: true },
+                                    course_role:        { type: String, default: 'Student', required: true },
+                                    section_id:         { type: Schema.Types.ObjectId, ref: 'Courses.sections' },
                                 }],
 
 });
@@ -27,57 +23,56 @@ const userSchema = new Schema({
 
 const courseSchema = new Schema({
     
-    name:                       { type: String, required: true, unique: true },
+    course_num:                 { type: String, required: true, unique: true },
     display_name:               { type: String, required: true },
     semester:                   { type: String, required: true },
 
     sections:                   [{ 
-                                        name:               { type: String, required: true },
-                                        start_date:         { type: Date },
-                                        due_date:           { type: Date },
-                                        end_date:           { type: Date },
+                                    name:               { type: String, required: true },
+                                    start_date:         { type: Date },
+                                    due_date:           { type: Date },
+                                    end_date:           { type: Date },
                                 }],
 
-    assignments_by_category:    [{ 
-                                        category: { type: String, required: true },
-
-                                        assignments: [{
-                                                        name:               { type: String, required: true },
-                                                        start_date:         { type: Date, required: true },
-                                                        due_date:           { type: Date, required: true },
-                                                        end_date:           { type: Date, required: true },
-                                                        section_based:      { type: Boolean, required: true },
-                                                        description:        { type: String },
-                                                        auto_grader:        { type: Boolean, required: true },                                     
+    assignments:                [{
+                                    category:           { type: String, required: true },
+                                    name:               { type: String, required: true },
+                                    start_date:         { type: Date, required: true },
+                                    due_date:           { type: Date, required: true },
+                                    end_date:           { type: Date, required: true },
+                                    section_based:      { type: Boolean, required: true },
+                                    description:        { type: String },
+                                    auto_grader:        { type: Boolean, required: true },                                     
                                                             
-                                                        form: {
-                                                                            file:               { type: Boolean, required: true },
-                                                                            file_name:          { type: String },
-                                                                            form_name:          { type: String },
-                                                                            text_fields:        { type: [String] },
-                                                                            dropdown: [{
-                                                                                                _id:                    false,
-                                                                                                name:                   { type: String, required: true },
-                                                                                                options:                { type: [String], required: true },
-                                                                            }],
-                                                                            checklist: [{
-                                                                                                _id:                    false,
-                                                                                                name:                   { type: String, required: true },
-                                                                                                select_multiple:        { type: Boolean },
-                                                                                                options:                { type: [String], required: true }
-                                                                            }],
+                                    form:               {
+                                                            file:               { type: Boolean, required: true },
+                                                            file_name:          { type: String },
+                                                            form_name:          { type: String },
+                                                            text_fields:        { type: [String] },
+
+                                                            dropdown: [{
+                                                                            _id:            false,
+                                                                            name:           { type: String, required: true },
+                                                                            options:        { type: [String], required: true },
+                                                                      }],
+
+                                                            checklist: [{
+                                                                            _id:                    false,
+                                                                            name:                   { type: String, required: true },
+                                                                            select_multiple:        { type: Boolean },
+                                                                            options:                { type: [String], required: true }
+                                                                        }],
                                                         },
                                                             
-                                                        problems: [{
-                                                                            problem_name:       { type: String, required: true },
-                                                                            score:              { type: Number, required: true },
+                                    problems:           [{
+                                                            problem_name:       { type: String, required: true },
+                                                            score:              { type: Number, required: true },
                                                         }],
                                                             
-                                                        late_penalty:       { type: Number },
-                                                        max_over_latest:    { type:Boolean, default: true },
-                                        }], 
+                                    late_penalty:       { type: Number },
+                                    max_over_latest:    { type:Boolean, default: true },
 
-                                }],
+                                }], 
 
 });
 
@@ -86,16 +81,16 @@ const submissionSchema = new Schema({
 
     user_id:                    { type: Schema.Types.ObjectId, ref: 'Users', required: true },   
     course_id:                  { type: Schema.Types.ObjectId, ref: 'Courses', required: true },
-    assignment_id:              { type: Schema.Types.ObjectId, ref: 'Courses.assignments_by_category.assignments', required: true },            
+    assignment_id:              { type: Schema.Types.ObjectId, ref: 'Courses.assignments', required: true },            
     version:                    { type: Number, required: true },
     file_name:                  { type: String, required: true },
     grader:                     { type: String, required: true },
     form_data:                  { type: String, required: true },
 
     scores:                     [{
-                                            _id:            false,
-                                            problem_id:     { type: Schema.Types.ObjectId, ref: 'Courses.assignments_by_category.assignments.problems', required: true },
-                                            score:          { type: Number, required: true },
+                                     _id:            false,
+                                     problem_id:     { type: Schema.Types.ObjectId, ref: 'Courses.assignments.problems', required: true },
+                                     score:          { type: Number, required: true },
                                 }],
 
     feedback:                   { type: String, required: true },
