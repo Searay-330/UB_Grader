f80722488f378789cf079a47fb790456c2f367b3
Richard Hanulewicz <rshanule@buffalo.edu>
2017-09-19 19:36:02 -0400

    Initial commit for Assignment page. Displays URL param as title! Wow!

---
+++ client/modules/Assignment/Assignment.js
@@ -0,0 +1,63 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+//Import Actions
+// import {method} from './AssignmentActions'
+
+// Import Style
+// import styles from './Assignment.css';
+
+// Import Components
+// Import Category from './componets/Category/Category';
+// Import Bootstrap
+import { Grid,Row,Col,PanelGroup, Panel, ListGroup, ListGroupItem} from 'react-bootstrap';
+
+
+
+export class Assignment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isMounted: false };
+  }
+
+  componentDidMount() {
+    this.setState({isMounted: true}); // eslint-disable-line
+  }
+
+
+  render() {
+    return (
+      
+      <div>
+        <h2>{this.props.params.assignment}</h2>
+      </div>
+   
+     /*<PanelGroup>
+      <Grid>
+        <Row>
+           <Category name={this.props.categories[0]}/>
+           <Category name={this.props.categories[1]}/>
+           <Category name={this.props.categories[2]}/>
+         </Row>
+      </Grid>
+      
+    </PanelGroup>*/
+    );
+  }
+}
+
+// Retrieve data from store as props
+function mapStateToProps(state) {
+  return {
+    categories: state.assignment.categories,
+  };
+}
+
+function mapDispatchToProps(dispatch) {  
+  return bindActionCreators({
+  }, dispatch);
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Assignment);
+
--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './modules/App/App';
 import Bs from './modules/bs/Bs';
 import Assignments from './modules/Assignments/Assignments';
+import Assignment from './modules/Assignment/Assignment';
 
 // require.ensure polyfill for node
 if (typeof require.ensure !== 'function') {
@@ -26,6 +27,7 @@ export default (
   <Route path="/" component={App}>
     <Route path="testing" component={Bs}/>
     <Route path="courses/:course/assignments" component={Assignments}/>
+    <Route path="courses/:course/assignments/:assignment" component={Assignment}/>
   </Route>
 
 );
