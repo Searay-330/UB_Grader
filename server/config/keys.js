bee6cef853b40fc63ead594a88e2a98e55966673
Aamel Unia <aameluni@buffalo.edu>
2017-09-20 11:43:52 -0400

    Google OAuth login and lgout functionality set up

--- server/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    googleClientID: '278182020302-jdj682j6t5kepdvc4va979ndovge2hlq.apps.googleusercontent.com',
-    googleClientSecret: 'HPiIeUHNXBkFYE-KTWrxPu6Y',
-    cookieKey: 'jkldjgkajiiejflkiwejginwelknmsdnmefnlajknffsnfioejmcaapim'
-}
