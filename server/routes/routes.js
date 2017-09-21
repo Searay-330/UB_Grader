000a41084061c1a03bb9cbb869df7150139539be
Aamel Unia <aameluni@buffalo.edu>
2017-09-21 19:53:27 -0400

    First 2 backend routes mentioned in the issue completed

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import * as AuthController from '../controllers/authentication'
+import * as AuthController from '../controllers/AuthenticationController'
+import * as CourseController from '../controllers/CourseController'
+import * as AuthCheck from '../util/authentication'
 const router = new Router();
 const passport = require('passport');
 
@@ -10,7 +12,7 @@ router.get('/auth/google', passport.authenticate('google', {
 );
 
 //Redirects user upon successful Google OAuth Login
-router.get('/auth/google/callback', passport.authenticate('google'),AuthController.loginRedirect);
+router.get('/auth/google/callback', passport.authenticate('google'), AuthController.loginRedirect);
 
 //Sends back the user that is currently logged in.
 router.get('/current_user', 		AuthController.getCurrentUser);
@@ -18,5 +20,10 @@ router.get('/current_user', 		AuthController.getCurrentUser);
 //Logs out user
 router.get('/logout',				AuthController.logoutUser);
 
+//Returns the courses for the user depending on access level.
+router.get('/courses', 				AuthCheck.isAuthenticated, CourseController.getCourses);
+
+//Returns the assignments of a specific course that user is enrolled in.
+router.get('/:course_num/assignments',	AuthCheck.isAuthenticated, CourseController.getAssignments);
 
 export default router;
