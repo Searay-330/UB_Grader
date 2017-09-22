73536fc6078bc31073ee8889d7f9900af8d5365c
Adhish Chugh <adhishchugh@icloud.com>
2017-09-21 21:15:01 -0400

    Added all routes for prototype phase

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,3 +1,4 @@
+import User from '../models/User'
 /**
  * Get user that is currently logged in
  * @param req
@@ -27,4 +28,18 @@ export function logoutUser(req,res) {
  */
 export function loginRedirect(req,res) {
     res.redirect('/courses');
+}
+
+
+/**
+ * Returns information about a specific user
+ * @param req
+ * @param res
+ * @returns void
+ */
+export function getUser(req,res) {
+    User.findById(req.params.user_id, (err, user) => {
+        if (err) res.status(500).send(err);            
+        res.send(user);        
+    });
 }
