117f0239c1c280278a80833ff346488d892eb359
Adhish Chugh <adhishchugh@icloud.com>
2017-09-21 21:15:01 -0400

    Added all routes for prototype phase

--- server/controllers/CourseController.js
+++ server/controllers/CourseController.js
@@ -8,17 +8,18 @@ import * as AuthCheck from '../util/authentication'
  * @param res : The response back to the caller.
  * Sends a list of courses back as the response (a list of json objects)
  */
-export function getCourses(req,res) {
+export function getCourses(req, res) {
 
-    var sys_role = AuthCheck.getAccessLevel(req,res);
-    if (sys_role){
-        if (sys_role === 'admin'){
+    var sys_role = AuthCheck.getAccessLevel(req, res);
+    if (sys_role) {
+        if (sys_role === 'admin') {
 
             Course.find({}, (err, allCourses) => {
                 if (err) res.status(500).send(err);
                 var courses = [];                
-                for (var i=0; i<allCourses.length; i++){
+                for (var i=0; i<allCourses.length; i++) {
                     courses.push({
+                        id:             allCourses[i].id,
                         course_num:     allCourses[i].course_num,
                         display_name:   allCourses[i].display_name,
                         semester:       allCourses[i].semester
@@ -38,6 +39,7 @@ export function getCourses(req,res) {
                 courses.forEach((courseobj) => {
                     if (course.course_id == courseobj.id){
                         courselist.push({
+                            id:                 courseobj.id,
                             course_num:         courseobj.course_num,
                             display_name:       courseobj.display_name,
                             semester:           courseobj.semester
@@ -54,13 +56,76 @@ export function getCourses(req,res) {
 
 /**
  * Gets all the assignments of a specific course.
- * @param req : User's request
+ * @param req : User's request (should contain course_id as a parameter)
  * @param res : The response back to the caller.
  * Sends a list of the assignments back as the response (a list of json objects)
  */
 
 export function getAssignments(req, res){
-    Course.findOne({'course_num':req.params.course_num}, (err,course) =>{
+    Course.findById(req.params.course_id, (err, course) =>{
+        if (err) res.status(500).send(err);                    
         res.send(course.assignments);
     })
+}
+
+
+/**
+ * Gets all the students of a specific course.
+ * @param req : User's request (should contain course_id as a parameter)
+ * @param res : The response back to the caller.
+ * Sends a list of the students back as the response (a list of json objects)
+ */
+
+export function getStudents(req, res){
+    var studentList = [];
+    User.find({}, (err, users) => {
+        if (err) res.status(500).send(err);            
+        users.forEach((user) => {
+            user.courses.forEach((course) => {
+                if (course.course_id == req.params.course_id && course.course_role == 'Student'){
+                    studentList.push(user);
+                }
+            });
+        });
+        res.send(studentList);
+    });
+}
+
+
+/**
+ * Gets all the sections of a specific course.
+ * @param req : User's request (should contain course_id as a parameter)
+ * @param res : The response back to the caller.
+ * Sends a list of the sections back as the response (a list of json objects)
+ */
+
+export function getSections(req, res){
+    var sectionList = [];
+    Course.findById(req.params.course_id, (err,course) =>{
+        if (err) res.status(500).send(err);                    
+        res.send(course.sections);
+    })
+}
+
+
+/**
+ * Gets all the students of a specific section of a course.
+ * @param req : User's request (should contain course_id and section_id as a parameter)
+ * @param res : The response back to the caller.
+ * Sends a list of the students back as the response (a list of json objects)
+ */
+
+export function getSectionStudents(req, res){
+    var studentList = [];
+    User.find({}, (err, users) => {
+        if (err) res.status(500).send(err);            
+        users.forEach((user) => {
+            user.courses.forEach((course) => {
+                if (course.course_id == req.params.course_id && course.section_id == req.params.section_id && course.course_role == 'Student'){
+                    studentList.push(user);
+                }
+            });
+        });
+        res.send(studentList);
+    });
 }
