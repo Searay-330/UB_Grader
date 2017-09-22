ecd71465de9fd4d2a875357c617ae94fa3959f69
Adhish Chugh <adhishchugh@icloud.com>
2017-09-22 16:03:41 -0400

    Fixed a BS Reduce bug

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './modules/App/App';
-import Bs from './modules/bs/Bs';
+import Bs from './modules/bs/bs';
 
 // require.ensure polyfill for node
 if (typeof require.ensure !== 'function') {
