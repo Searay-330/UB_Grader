058495bec1db027b21ee79c2e415bc66d11853ce
Aamel Unia <aameluni@buffalo.edu>
2017-10-01 18:10:10 -0400

    fixed error handling

--- server/controllers/CourseController.js
+++ server/controllers/CourseController.js
@@ -12,46 +12,54 @@ export function getCourses(req, res) {
 
     var sys_role = AuthCheck.getAccessLevel(req, res);
     if (sys_role) {
+
         if (sys_role === 'admin') {
 
             Course.find({}, (err, allCourses) => {
-                if (err) return res.status(500).send(err);
-                var courses = [];                
-                for (var i=0; i<allCourses.length; i++) {
-                    courses.push({
-                        id:             allCourses[i].id,
-                        course_num:     allCourses[i].course_num,
-                        display_name:   allCourses[i].display_name,
-                        semester:       allCourses[i].semester
-                    });
+                if (err){
+                    res.status(500).send(err);
                 }
-                res.status(200).send(courses);  
+                else {  
+                    var courses = [];                
+                    for (var i=0; i<allCourses.length; i++) {
+                        courses.push({
+                            id:             allCourses[i].id,
+                            course_num:     allCourses[i].course_num,
+                            display_name:   allCourses[i].display_name,
+                            semester:       allCourses[i].semester
+                        });
+                    }
+                    res.status(200).send(courses); 
+                }            
             });            
         }
     }
+
     else{
 
         var courselist = [];
 
         Course.find({}, (err, courses) => {
-            if (err) return res.status(500).send(err);            
-            req.user.courses.forEach((course) => {
-                courses.forEach((courseobj) => {
-                    if (course.course_id == courseobj.id){
-                        courselist.push({
-                            id:                 courseobj.id,
-                            course_num:         courseobj.course_num,
-                            display_name:       courseobj.display_name,
-                            semester:           courseobj.semester
-                        });
-                    }
+            if (err) {
+                res.status(500).send(err);
+            }
+            else {          
+                req.user.courses.forEach((course) => {
+                    courses.forEach((courseobj) => {
+                        if (course.course_id == courseobj.id){
+                            courselist.push({
+                                id:                 courseobj.id,
+                                course_num:         courseobj.course_num,
+                                display_name:       courseobj.display_name,
+                                semester:           courseobj.semester
+                            });
+                        }
+                    });
                 });
-            });
-            res.status(200).send(courselist);
+                res.status(200).send(courselist);
+            }
         });
-
     }
-
 }
 
 /**
@@ -63,9 +71,9 @@ export function getCourses(req, res) {
 
 export function getAssignments(req, res){
     Course.findOne({ 'course_num': req.params.course_num }, (err, course) => {
-        if (err) return res.status(500).send(err);                    
-        res.status(200).send(course.assignments);
-    })
+        if (err) res.status(500).send(err);                    
+        else res.status(200).send(course.assignments);
+    });
 }
 
 
@@ -79,15 +87,19 @@ export function getAssignments(req, res){
 export function getStudents(req, res){
     var studentList = [];
     User.find({}, (err, users) => {
-        if (err) return res.status(500).send(err);            
-        users.forEach((user) => {
-            user.courses.forEach((course) => {
-                if (course.course_num == req.params.course_num && course.course_role == 'Student'){
-                    studentList.push(user);
-                }
+        if (err) {
+            res.status(500).send(err); 
+        }
+        else {          
+            users.forEach((user) => {
+                user.courses.forEach((course) => {
+                    if (course.course_num == req.params.course_num && course.course_role == 'Student'){
+                        studentList.push(user);
+                    }
+                });
             });
-        });
-        res.status(200).send(studentList);
+            res.status(200).send(studentList);
+        }
     });
 }
 
@@ -102,8 +114,8 @@ export function getStudents(req, res){
 export function getSections(req, res){
     var sectionList = [];
     Course.findOne({ 'course_num': req.params.course_num }, (err,course) =>{
-        if (err) return res.status(500).send(err);                    
-        res.status(200).send(course.sections);
+        if (err) res.status(500).send(err);                    
+        else res.status(200).send(course.sections);
     })
 }
 
@@ -118,15 +130,19 @@ export function getSections(req, res){
 export function getSectionStudents(req, res){
     var studentList = [];
     User.find({}, (err, users) => {
-        if (err) return res.status(500).send(err);            
-        users.forEach((user) => {
-            user.courses.forEach((course) => {
-                if (course.course_num == req.params.course_num && course.section_name == req.params.section_name && course.course_role == 'Student'){
-                    studentList.push(user);
-                }
+        if (err) {
+            res.status(500).send(err);
+        }
+        else {           
+            users.forEach((user) => {
+                user.courses.forEach((course) => {
+                    if (course.course_num == req.params.course_num && course.section_name == req.params.section_name && course.course_role == 'Student'){
+                        studentList.push(user);
+                    }
+                });
             });
-        });
-        res.status(200).send(studentList);
+            res.status(200).send(studentList);
+        } 
     });
 }
 
@@ -142,8 +158,8 @@ export function createCourse(req, res){
     // if (sys_role === 'admin'){
     var course = new Course(req.body);        
     course.save((err, courseobj) => {
-        if (err) return res.status(500).send(err);
-        res.status(200).send(courseobj);
+        if (err) res.status(500).send(err);
+        else res.status(200).send(courseobj);
     });
     // }
 }
@@ -159,7 +175,7 @@ export function updateCourse(req, res){
     // var sys_role = AuthCheck.getAccessLevel(req, res);
     // if (sys_role === 'admin'){
     Course.findOne({ 'course_num': req.params.course_num }, (err,courseobj) =>{
-        if (err) return res.status(500).send(err); 
+        if (err) res.status(500).send(err); 
         // for (var key in req.body){
         //     courseobj.key = req.body[key];
         //     console.log(courseobj.key);
@@ -168,20 +184,22 @@ export function updateCourse(req, res){
         //     console.log(courseobj[key]);
         //     console.log('-----------');
         // }
-        if (req.body.display_name){
-            courseobj.display_name = req.body.display_name;
+        else {
+            if (req.body.display_name){
+                courseobj.display_name = req.body.display_name;
+            }
+            if (req.body.semester){
+                courseobj.semester = req.body.semester;
+            }                   
+            if (req.body.sections){
+                courseobj.sections = req.body.sections;
+            }
+            courseobj.save((err, updatedcourseobj) => {
+                if (err) res.status(500).send(err);
+                else res.status(200).send(updatedcourseobj);
+            });
         }
-        if (req.body.semester){
-            courseobj.semester = req.body.semester;
-        }                   
-        if (req.body.sections){
-            courseobj.sections = req.body.sections;
-        }
-        courseobj.save((err, updatedcourseobj) => {
-            if (err) return res.status(500).send(err);
-            res.status(200).send(updatedcourseobj);
-        });
-    })
+    });
 
     //}
 }
@@ -194,25 +212,33 @@ export function updateCourse(req, res){
  */
 export function addCourseToUser(req,res){
     User.findOne({'email': req.params.student_email}, (err, userobj) => {
-        if (err) return res.status(500).send(err);                    
-        Course.findOne({'course_num': req.params.course_num}, (err, courseobj) => {
-            if (err) return res.status(500).send(err);
-            if (courseobj) {            
-                var course_info = {
-                                    course_id:    courseobj.id,
-                                    course_num:   courseobj.course_num,
-                                    course_role:  req.body.course_role
-                                  }
-                userobj.courses.addToSet(course_info);
-                userobj.save((err, updateduserobj) => {
-                    if (err) return res.status(500).send(err);                
-                    res.status(200).send({Status: 200, Message: 'Successfully added '+userobj.email+' to '+courseobj.display_name});
-                });
-            }
-        else{
-            res.status(404).send({Status: 404, Message: 'Sorry, unable to find that course'});                              
+        if (err) {
+            res.status(500).send(err); 
         }
-        });
+        else{                   
+            Course.findOne({'course_num': req.params.course_num}, (err, courseobj) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else{
+                    if (courseobj) {            
+                        var course_info = {
+                                            course_id:    courseobj.id,
+                                            course_num:   courseobj.course_num,
+                                            course_role:  req.body.course_role
+                                        }
+                        userobj.courses.addToSet(course_info);
+                        userobj.save((err, updateduserobj) => {
+                            if (err) res.status(500).send(err);                
+                            else res.status(200).send({Status: 200, Message: 'Successfully added '+userobj.email+' to '+courseobj.display_name});
+                        });
+                    }
+                    else{
+                        res.status(404).send({Status: 404, Message: 'Sorry, unable to find that course'});                              
+                    }
+                }
+            });
+        }
     });
 }
 
@@ -224,54 +250,65 @@ export function addCourseToUser(req,res){
  */
 export function addUserToSection(req,res){
     User.findOne({'email': req.params.student_email}, (err, userobj) => {
-        if (err) return res.status(500).send(err);                    
-        Course.findOne({'course_num': req.params.course_num}, (err, courseobj) => {
-            if (err) return res.status(500).send(err);
-            if (!courseobj || !userobj) {
-                return res.status(404).send({Status: 404, Message: 'Sorry, unable to find that user and/or course'});  
-            }
-            else {           
-                courseobj.sections.forEach((section) => {
-                    if (section.name == req.params.section_name){
-                        userobj.courses.forEach((course) => {
-                            if (course.course_num == req.params.course_num){
-                                course.section_id = section.id;
-                                course.section_name = section.name;
-                                userobj.save((err, updateduserobj) => {
-                                    if (err) return res.status(500).send(err);                
-                                    return res.status(200).send(updateduserobj);
-                                });
-                            }
-                        });
-                    }
-                });
-            res.status(500).send({Status: 500, Message: 'Sorry, unable to add user to that section'});  
+        if (err) {
+            res.status(500).send(err);    
         }
-        });
+        else{                
+            Course.findOne({'course_num': req.params.course_num}, (err, courseobj) => {
+                var isInSection = false;
+                if (err) {
+                    res.status(500).send(err);
+                }
+                if (!courseobj || !userobj) {
+                    res.status(404).send({Status: 404, Message: 'Sorry, unable to find that user and/or course'});  
+                }
+                else {           
+                    courseobj.sections.forEach((section) => {
+                        if (section.name == req.params.section_name){
+                            userobj.courses.forEach((course) => {
+                                if (course.course_num == req.params.course_num){
+                                    course.section_id = section.id;
+                                    course.section_name = section.name;
+                                    isInSection = true;
+                                    userobj.save((err, updateduserobj) => {
+                                        if (err) res.status(500).send(err);                
+                                        else res.status(200).send(updateduserobj);
+                                    });
+                                }
+                            });
+                        }
+                    });
+                    if (!isInSection) res.status(500).send({Status: 500, Message: 'Sorry, unable to add user to that section'});  
+                }
+            });
+        } 
     });
 }
 
 export function removeCourseFromUser(req,res){
     User.findOne({'email': req.params.student_email}, (err, userobj) => {
-        if (err) return res.status(500).send(err);
-        if (userobj) {
-            console.log(userobj);
-            var isEnrolled = false;
-            userobj.courses.forEach((course) => {
-                if (course.course_num == req.params.course_num){
-                    isEnrolled = true;
-                    var index = userobj.courses.indexOf(course);
-                    userobj.courses.splice(index,1);
-                    userobj.save((err, updateduserobj) => {
-                        if (err) return res.status(500).send(err);                
-                        res.status(200).send(updateduserobj);
-                    });            
-                }
-            });
-            if(!isEnrolled) res.status(500).send({Status: 500, Message: 'Sorry, unable to remove user from course'});                              
+        var isEnrolled = false;        
+        if (err) {
+            res.status(500).send(err);
         }
         else{
-            return res.status(404).send({Status: 404, Message: 'Sorry, unable to find that user'});   
-        }                       
+            if (userobj) {
+                userobj.courses.forEach((course) => {
+                    if (course.course_num == req.params.course_num){
+                        isEnrolled = true;
+                        var index = userobj.courses.indexOf(course);
+                        userobj.courses.splice(index,1);
+                        userobj.save((err, updateduserobj) => {
+                            if (err) return res.status(500).send(err);                
+                            res.status(200).send(updateduserobj);
+                        });            
+                    }
+                });
+            }
+            else{
+                return res.status(404).send({Status: 404, Message: 'Sorry, unable to find that user'});   
+            }
+        }
+        if(!isEnrolled) res.status(500).send({Status: 500, Message: 'Sorry, unable to remove user from course'});                                      
     });
 }
