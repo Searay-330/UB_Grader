42ecea66e4edf047261d0f91e16259c5648a6b2f
David Schuster <searay330@imageknight.com>
2017-10-05 18:42:37 -0400

    handling state is now done much nicer

--- client/modules/Assignments/components/CreateAssignment/CreateAssignment.js
+++ client/modules/Assignments/components/CreateAssignment/CreateAssignment.js
@@ -20,6 +20,7 @@ import Paper from 'material-ui/Paper';
 import Divider from 'material-ui/Divider';
 import DatePicker from 'material-ui/DatePicker';
 import FlatButton from 'material-ui/FlatButton';
+import AutoComplete from 'material-ui/AutoComplete';
 import TimePicker from 'material-ui/TimePicker';
 
 
@@ -29,13 +30,14 @@ export class CreateAssignment extends Component {
     super(props);
     this.state = {
       displayName: '',
-      startDate: '',
-      dueDate: '',
-      endDate: '',
-      startTime: '',
-      dueTime: '',
-      endTime: '',
-
+      startDate: {},
+      dueDate: {},
+      endDate: {},
+      startTime: {},
+      dueTime: {},
+      endTime: {},
+      category: '',
+      name: '',
     };
   }
 
@@ -46,47 +48,87 @@ export class CreateAssignment extends Component {
   componentWillReceiveProps(nextProps) {
 
   }
-   handleChange = (event) => {
-    this.setState({
-      displayName: document.getElementById("displayName").value,
-      startDate: document.getElementById("startDate").value,
-      dueDate: document.getElementById("dueDate").value,
-      endDate: document.getElementById("endDate").value,
-      startTime: document.getElementById("startTime").value,
-      dueTime: document.getElementById("dueTime").value,
-      endTime: document.getElementById("endTime").value,
-    });
- 
+
+  wrapperFunction = (location)=>{return (event,date) => {this.dateChange(event,date,location)}};
+
+  dateChange = (event, date, location) => {
+    var st  = getEditableState(this.state);
+    st.startDate = (location == "startDate") ? date : st.startDate;
+    st.dueDate = (location == "dueDate") ? date : st.dueDate;
+    st.endDate = (location == "endDate") ? date : st.endDate;
+    st.startTime = (location == "startTime") ? date : st.startTime;
+    st.dueTime = (location == "dueTime") ? date : st.dueTime;
+    st.endTime = (location == "endTime") ? date : st.endTime;
+    this.setState(st);
+  };
+
+  otherChange = (event) => {
+      var st = getEditableState(this.state);
+      st.displayName = document.getElementById("displayName").value;
+      st.name = document.getElementById("name").value;
+      st.category = (typeof event == "string") ? event : st.category;
+      this.setState(st);
   };
 
   render() {
     return (
       <div>
-       <Card>
+       <Card initiallyExpanded={true}>
     <CardHeader
       title="Basic Settings"
       actAsExpander={true}
       showExpandableButton={true}
     />
-    <CardActions>
-    </CardActions>
     <CardText expandable={true}>
-    <TextField id="displayName" value={this.state.displayName} onChange={this.handleChange} className={styles.dividerUnder} hintText="Display Name" floatingLabelText="Assignment Name"/>
-      <Divider />
-      <DatePicker id="startDate" value={this.state.startDate} onChange={this.handleChange} style={{display: 'inline-block'}} floatingLabelText= "Start Date" hintText="Start Date" autoOk={true}/>
-      <TimePicker id="startTime" value={this.state.startTime} onChange={this.handleChange} className = {styles.adjust} style={{display: 'inline-block'}} floatingLabelText="Start Time" hintText="Start Time" autoOk={true}/>
+    <TextField id="displayName" value={this.state.displayName} onChange={this.otherChange}  hintText="Display Name" floatingLabelText="Assignment Display Name"/>
+    <TextField id="name" value={this.state.name} onChange={this.otherChange} className = {styles.adjust} hintText="Name" floatingLabelText="Assignment Name"/>
       <br />
-      <DatePicker id="dueDate" value={this.state.dueDate} onChange={this.handleChange} style={{display: 'inline-block'}} floatingLabelText= "Due Date" hintText="Due Date" autoOk={true}/>
-      <TimePicker id="dueTime" value={this.state.dueTime} onChange={this.handleChange} className = {styles.adjust} style={{display: 'inline-block'}} floatingLabelText="Due Time" hintText="Due Time" autoOk={true}/>
+      <DatePicker id="startDate" value={this.state.startDate} onChange={this.wrapperFunction("startDate")} style={{display: 'inline-block'}} floatingLabelText= "Start Date" hintText="Start Date" autoOk={true}/>
+      <TimePicker id="startTime" value={this.state.startTime} onChange={this.wrapperFunction("startTime")} className = {styles.adjust} style={{display: 'inline-block'}} floatingLabelText="Start Time" hintText="Start Time"/>
       <br />
-      <DatePicker id="endDate" value={this.state.endDate} onChange={this.handleChange} style={{display: 'inline-block'}} floatingLabelText= "End Date" hintText="End Date" autoOk={true}/>
-      <TimePicker id="endTime" value={this.state.endTime} onChange={this.handleChange} className = {styles.adjust} style={{display: 'inline-block'}} floatingLabelText="End Time" hintText="End Time" autoOk={true}/>
+      <DatePicker id="dueDate" value={this.state.dueDate} onChange={this.wrapperFunction("dueDate")} style={{display: 'inline-block'}} floatingLabelText= "Due Date" hintText="Due Date" autoOk={true}/>
+      <TimePicker id="dueTime" value={this.state.dueTime} onChange={this.wrapperFunction("dueTime")} className = {styles.adjust} style={{display: 'inline-block'}} floatingLabelText="Due Time" hintText="Due Time"/>
+      <br />
+      <DatePicker id="endDate" value={this.state.endDate} onChange={this.wrapperFunction("endDate")} style={{display: 'inline-block'}} floatingLabelText= "End Date" hintText="End Date" autoOk={true}/>
+      <TimePicker id="endTime" value={this.state.endTime} onChange={this.wrapperFunction("endTime")} className = {styles.adjust} style={{display: 'inline-block'}} floatingLabelText="End Time" hintText="End Time" />
+      <br />
+      <AutoComplete id="category" value={this.state.category} onUpdateInput={this.otherChange} floatingLabelText="Category" filter={AutoComplete.noFilter} openOnFocus={true} dataSource = {["hello"]}/>
       <br />
       <br />
       <br />
-      <RaisedButton label='Create Assignment' primary={true} />
     </CardText>
   </Card>
+
+  <br />
+
+  <Card initiallyExpanded={false}>
+    <CardHeader
+      title="Autograder Settings"
+      actAsExpander={true}
+      showExpandableButton={true}
+    />
+    <CardText expandable={true}>
+
+    </CardText>
+  </Card>
+
+  <br />
+  <Card initiallyExpanded={false}>
+    <CardHeader
+      title="Problem Settings"
+      actAsExpander={true}
+      showExpandableButton={true}
+    />
+    <CardText expandable={true}>
+
+    </CardText>
+  </Card>
+
+
+    <br />
+
+
+      <RaisedButton label='Create Assignment' onClick={()=>{console.log(this.state)}} primary={true} />
       </div>
     );
   }
@@ -103,6 +145,21 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators({
 
   }, dispatch);
+}
+
+function getEditableState(state){
+  return ({
+
+      displayName: state.displayName,
+      startDate: state.startDate,
+      dueDate: state.dueDate,
+      endDate: state.endDate,
+      startTime: state.startTime,
+      dueTime: state.dueTime,
+      endTime: state.endTime,
+      category: state.category,
+      name: state.name,
+    });
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateAssignment);
