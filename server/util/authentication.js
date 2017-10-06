ffd8456a2d3c6659a17c8be4c53de6d596eed89e
Aamel Unia <aameluni@buffalo.edu>
2017-10-06 18:23:24 -0400

    configured auth util functions to always send back a JSON

--- server/util/authentication.js
+++ server/util/authentication.js
@@ -1,11 +1,11 @@
 export function isAuthenticated(req, res, next){
     if (req.user) return next();
-    res.redirect('/');
+    else res.status(401).send({status: 401, message: 'Sorry, you must be logged in to view this page'});
 }
 
 export function isAdmin(req, res, next){
     if (req.user.sys_role == 'admin') return next();
-    res.redirect('/');
+    else res.status(403).send({status: 403, message: 'Sorry, you are not authorized to view this page'});
 }
 
 export function isInstructor(req, res, next){
@@ -18,5 +18,5 @@ export function isInstructor(req, res, next){
             }
         }
     }
-    res.redirect('/');    
+    res.status(403).send({status: 403, message: 'Sorry, you are not authorized to view this page'});
 }
