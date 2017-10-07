8652418a8c2f085b07c2e489dae76018904d3d53
David Schuster <searay330@imageknight.com>
2017-10-07 00:48:03 -0400

    added better problem input validation

--- client/modules/Assignments/components/CreateAssignment/CreateAssignmentActions.js
+++ client/modules/Assignments/components/CreateAssignment/CreateAssignmentActions.js
@@ -21,21 +21,35 @@ export function resetError(){
 }
 
 export function submitForm(formData, course) {
-	console.log(formData);
   return function (dispatch) {
     dispatch(resetError());
   	var hold = undefined;
 	var data;
 
 	data = new FormData();
-	hold = (!hold) ? (formData.name.replace(/[^A-Za-z0-9._~:\.\/\[\]@!$&'()*+,;=`\-_]/, '') != "") ? data.append("assignment_num", encodeURIComponent(formData.name)) : "Assignment Name Is A Required Field Or Your String Contained No Urlable Chars" : hold;
+	// hold = (!hold) ? (formData.name.replace(/[^A-Za-z0-9._~:\.\/\[\]@!$&'()*+,;=`\-_]/, '') != "") ? data.append("assignment_num", encodeURIComponent(formData.name)) : "Assignment Name Is A Required Field Or Your String Contained No Urlable Chars" : hold;
 	hold = (!hold) ? (document.getElementById("tar") != null) ? data.append("tar", document.getElementById("tar").files[0], "tarball.tar") : hold : hold;
 	hold = (!hold) ? (document.getElementById("make") != null) ? data.append("make", document.getElementById("make").files[0], "makefile") : hold : hold;
 	hold = (!hold) ? (formData.category != "") ? data.append("category", formData.category) : "Category Is A Required Field" : hold;
 	hold = (!hold) ? (formData.displayName != "") ? data.append("name", formData.displayName) : "Display Name Is A Required Field" : hold;
 	data.append("section_based",false);
 	data.append("auto_grader",true);
-	hold = (!hold) ? (formData.p_name != "" && parseInt(formData.max_score) != NaN) ? data.append("problems", JSON.stringify([{problem_name:formData.p_name, score : parseInt(formData.max_score) }])) : hold : hold;
+	if(!hold){
+		if(formData.p_name != "" && !isNaN(parseInt(formData.max_score))){
+			data.append("problems", JSON.stringify([{problem_name:formData.p_name, score : parseInt(formData.max_score) }]));
+		}
+		if(formData.p_name != "" && isNaN(parseInt(formData.max_score))){
+			hold = "Please Enter A Vaild Number For Score";
+		}
+		if(formData.p_name == "" && !isNaN(parseInt(formData.max_score))){
+			hold = "Plesea Enter A Vaild Name For The Problem";
+		}
+		if(formData.p_name == "" && isNaN(parseInt(formData.max_score)) && formData.max_score != ""){
+			hold = "Plesea Enter A Vaild Name For The Problem And A Vaild Score";
+		}
+
+	}
+
 	if(hold){
 		return dispatch(throwError(hold));
 	}
