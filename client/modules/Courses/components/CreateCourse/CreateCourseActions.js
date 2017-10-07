bd3122b30f07c7de79da204d1640b822d169b246
Richard Hanulewicz <rshanule@buffalo.edu>
2017-10-07 18:36:02 -0400

    Adding instructor via email functional on course creation.

--- client/modules/Courses/components/CreateCourse/CreateCourseActions.js
+++ client/modules/Courses/components/CreateCourse/CreateCourseActions.js
@@ -3,19 +3,18 @@ import callApi, {callApiWithFiles} from '../../../../util/apiCaller';
 export function submitForm(formData) {
 	var data;
   	data = {course_num: formData.course_num, display_name: formData.display_name, semester: formData.semester};
-  	
-  	// var instructorData = new FormData();
-  	// instructorData.append("student_email", formData.student_email);
-  	// instructorData.append("course_role", 'instructor');
+
+  	var instructorData;
+  	instructorData = {student_email: formData.student_email, course_role: 'instructor'};
 
   	return function (dispatch) {
 	    dispatch(() => {return {type:"wait"};});
 	    return callApi("courses/create", 'post', data).then(data => {
 	        console.log(data);
-	     //    dispatch(() => {return {type:"wait"};});
-	    	// return callApi("courses/" + formData.course_num + "/enroll", 'post', instructorData).then(instructorData => {
-	     //    		console.log(instructorData);
-	     //  		})
+	        dispatch(() => {return {type:"wait"};});
+	    	return callApi("courses/" + formData.course_num + "/enroll", 'post', instructorData).then(instructorData => {
+	        		console.log(instructorData);
+	      		})
 	      })
   	}
 }
