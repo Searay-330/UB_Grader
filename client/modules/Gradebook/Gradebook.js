cf66dd7afdc8986ae78862a764a55ee89f370ec7
Theknickerbocker <kevinrat@buffalo.edu>
2017-10-09 02:44:38 -0400

    Created gradebook

---
+++ client/modules/Gradebook/Gradebook.js
@@ -0,0 +1,133 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { getStudentGrades, getProfessorGrades, getUserInfo } from './GradebookActions';
+
+import AssignmentGrade from './components/AssignmentGrade';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+} from 'material-ui/Table';
+
+import { PanelGroup, Grid, Row, Col } from 'react-bootstrap';
+
+export class Gradebook extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            render: false,
+            userData: {},
+            submissions: {},
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            render: false,
+            userData: {},
+            submissions: {},
+        })
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log("NEXT PROPS");
+        console.log(nextProps);
+        var courseNum = nextProps.params.course;
+        var email = nextProps.user.email;
+        console.log(nextProps.assignments);
+        for (var assignment in nextProps.assignments) {
+            var assignmentNum = nextProps.assignments[assignment].assignment_num;
+            console.log("Getting: " + courseNum + " " + assignmentNum + " " + email)
+            this.props.getStudentGrades(courseNum, assignmentNum, email).then(() => { this.forceUpdate() });
+        }
+        // console.log(this.props.submissions);
+    }
+
+    render() {
+
+        // console.log(this.props);
+        return (
+            this.studentBook()
+        );
+    }
+
+    instructorBook() {
+        return (
+            <Card>
+                <CardTitle title="Grades" />
+                <CardMedia>
+                    <Table>
+                        <TableHeader>
+
+                        </TableHeader>
+                        <TableBody>
+
+                        </TableBody>
+                    </Table>
+                </CardMedia>
+            </Card>
+        )
+    }
+
+    studentBook() {
+        console.log(Object.keys(this.props.submissions));
+        if (Object.keys(this.props.submissions) == 1) {
+            return null;
+        }
+        this.rows = [];
+        console.log(this.props);
+        for (var assignment in this.props.assignments) {
+            console.log(this.props.submissions);
+            console.log(this.props.assignments[assignment].assignment_num);
+            var key = this.props.assignments[assignment].assignment_num;
+            var data = this.props.submissions[key];
+            if (data == undefined) {
+                continue;
+            }
+            console.log(data);
+            this.rows.push(<AssignmentGrade name={key} data={data} />);
+        }
+        console.log(this.rows);
+        return (
+            <Card>
+                <CardTitle title="Grades" />
+                <CardMedia>
+                        <table style="width:100%">
+                            <tr>
+                                <th>Assignment</th>
+                                <th>Score</th>
+                                <th>Version</th>
+                            </tr>
+                            {this.rows}
+                        </table>
+                </CardMedia>
+            </Card>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    console.log(state);
+    return {
+        user: state.app.user,
+        assignments: state.assignments.assignmentsData,
+        submissions: state.gradebook.submissions,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        getStudentGrades: getStudentGrades,
+        getProfessorGrades: getProfessorGrades,
+        getUserInfo: getUserInfo,
+    }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Gradebook);
---
+++ client/modules/Gradebook/components/CreateAssignment/CreateAssignmentActions.js
@@ -0,0 +1,149 @@
+import callApi, {callApiWithFiles} from '../../../../util/apiCaller';
+
+
+
+export function getCategories(assignmentsData){
+	var categories = [];
+    for (var i = 0; i < assignmentsData.length; ++i) {
+      if (!categories.includes(assignmentsData[i].category)) {
+        categories.push(assignmentsData[i].category);
+      }
+    }
+	return categories;    
+}
+
+export function throwError(message){
+	return {type:"error", error: message};
+}
+
+
+export function resetError(){
+	return {type:"error", error:""};
+}
+
+export function redirect(){
+	return {type:"redir"};
+}
+
+
+export function redirectReset(){
+	return {type: "redirr"};
+}
+
+
+
+export function submitForm(formData, course) {
+  return function (dispatch) {
+    dispatch(resetError());
+  	var hold = undefined;
+	var data;
+
+
+
+	data = new FormData();
+
+	//must be FIRST
+	hold = (!hold) ? (formData.name.replace(/[^A-Za-z0-9._~:\.\/\[\]@!$&'()*+,;=`\-_]/, '') != "" && formData.name != "create") ? data.append("assignment_num", encodeURIComponent(formData.name)) : "Assignment Name Is A Required Field Or Your String Contained No Urlable Chars" : hold;
+
+	//dates
+	hold = validateDates(formData);
+	if(hold == "skip"){
+		hold = undefined;
+	}else{
+		if(hold){
+			return dispatch(throwError(hold));
+		}
+		formData.startDate.setHours(formData.startTime.getHours(),formData.startTime.getMinutes(),0, 0);
+		formData.dueDate.setHours(formData.dueTime.getHours(),formData.dueTime.getMinutes(),0, 0);
+		formData.endDate.setHours(formData.endTime.getHours(),formData.endTime.getMinutes(),0, 0);
+
+		if(formData.startDate.getTime() > formData.dueDate.getTime()){
+			return dispatch(throwError("Due Date Must Come After Start"));
+		}
+
+		if(formData.dueDate.getTime() > formData.endDate.getTime()){
+			return dispatch(throwError("End Date Must Come After Due"));
+		}
+
+		data.append("start_date", formData.startDate);
+		data.append("due_date", formData.dueDate);
+		data.append("end_date", formData.endDate);
+	}
+
+	// everything else
+	hold = (!hold) ? validateFiles() : hold;
+	hold = (!hold) ? data.append("make", document.getElementById("make").files[0]) : hold;
+	hold = (!hold) ? data.append("tar", document.getElementById("tar").files[0]) : (hold == "skip") ? undefined : hold;
+	hold = (!hold) ? (formData.category != "") ? data.append("category", formData.category) : "Category Is A Required Field" : hold;
+	hold = (!hold) ? (formData.displayName != "") ? data.append("name", formData.displayName) : "Display Name Is A Required Field" : hold;
+
+	data.append("section_based",false);
+	data.append("auto_grader", (document.getElementById("tar") != null && document.getElementById("make") != null && document.getElementById("tar").files[0] && document.getElementById("make").files[0]) ? true : false);
+
+	if(!hold){
+		if(formData.p_name != "" && !isNaN(parseInt(formData.max_score))){
+			data.append("problems", JSON.stringify([{problem_name:formData.p_name, score : parseInt(formData.max_score) }]));
+		}
+		if(formData.p_name != "" && isNaN(parseInt(formData.max_score))){
+			hold = "Please Enter A Vaild Number For Score";
+		}
+		if(formData.p_name == "" && !isNaN(parseInt(formData.max_score))){
+			hold = "Plesea Enter A Vaild Name For The Problem";
+		}
+		if(formData.p_name == "" && isNaN(parseInt(formData.max_score)) && formData.max_score != ""){
+			hold = "Plesea Enter A Vaild Name For The Problem And A Vaild Score";
+		}
+
+	}
+
+	if(hold){
+		return dispatch(throwError(hold));
+	}
+    return callApiWithFiles("courses/" + course + "/assignments/create", data)
+      .then(data => {
+      	if(data.hasOwnProperty("_message")){
+        dispatch(throwError(data["_message"]));
+      	}else{
+      		dispatch(redirect());
+      	}
+      })
+  }
+}
+
+function validateDates(formData){
+	if(formData.startDate == null && formData.startTime == null && formData.dueDate == null && formData.dueTime == null && formData.endDate == null && formData.endTime == null){
+		return "skip";
+	}
+	if(formData.startDate == null){
+		return "Please Enter A Vaild Start Date";
+	}
+	if(formData.startTime == null){
+		return "Please Enter A Vaild Start Time";
+	}
+	if(formData.dueDate == null){
+		return "Please Enter A Vaild Due Date";
+	}
+	if(formData.dueTime == null){
+		return "Please Enter A Vaild Due Time";
+	}
+	if(formData.endDate == null){
+		return "Please Enter A Vaild End Date";
+	}
+	if(formData.endTime == null){
+		return "Please Enter A Vaild End Time";
+	}
+	return undefined;
+}
+
+function validateFiles(){
+	if(document.getElementById("make") == null && document.getElementById("tar") == null){
+		return "skip";
+	}
+	if(document.getElementById("make").files[0] && !document.getElementById("tar").files[0]){
+		return "Please Input A Vaild Tar File";
+	}
+	if(!document.getElementById("make").files[0] && document.getElementById("tar").files[0]){
+		return "Please Input A Vaild Make File";
+	}
+	return undefined;
+}
