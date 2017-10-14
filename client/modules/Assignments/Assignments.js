5702b50248060294806b4075cac9e281d79d63e0
David Schuster <searay330@imageknight.com>
2017-10-14 18:02:52 -0400

    fixed route

--- client/modules/Assignments/Assignments.js
+++ client/modules/Assignments/Assignments.js
@@ -43,10 +43,10 @@ export class Assignments extends Component {
     var create2 = null;
     var gradebook = null;
     if(nextProps.perms[this.props.params.course] != "student"){
-      create = <MenuItem key={0} onClick={()=>{window.location = "/create"}}>Create Assignemnt</MenuItem>;
-      create2 = <MenuItem key={1} onClick={() => { window.location = /courses/ + this.props.params.course + "/adduser" }}>Add User To Course</MenuItem>;
+      create = <MenuItem key={0} onClick={()=>{window.location = "/courses/" + this.props.params.course + "/assignments/create"}}>Create Assignemnt</MenuItem>;
+      create2 = <MenuItem key={1} onClick={() => { window.location = "/courses/" + this.props.params.course + "/adduser" }}>Add User To Course</MenuItem>;
     } 
-    gradebook = <MenuItem key={2} onClick={() => { window.location = ("/courses/") + this.props.params.course + "/assignments/gradebook"}}>Gradebook</MenuItem>;
+    gradebook = <MenuItem key={2} onClick={() => { window.location = "/courses/" + this.props.params.course + "/assignments/gradebook"}}>Gradebook</MenuItem>;
     var elems = [create,create2, gradebook];
     nextProps.changeMenuItems(elems);
     }
