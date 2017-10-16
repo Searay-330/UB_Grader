ec8481c22b6fb63aa583bbe9ad9598bb05de76a8
Dhruv Kumar <dkumar2@buffalo.edu>
2017-10-16 00:54:01 -0400

    added route to get latest submission of all users in an assignment

--- server/controllers/SubmissionController.js
+++ server/controllers/SubmissionController.js
@@ -4,8 +4,7 @@ import Submission from '../models/Submission'
 import * as TangoController from './TangoController'
 import * as AuthCheck from '../util/authentication'
 
-const request = require('request');
-const fs = require('fs-extra');
+var async = require('async');
 
 /**
  * Gets all the user submissions of a specific assignment.
@@ -15,6 +14,7 @@ const fs = require('fs-extra');
  */
 
 export function getUserSubmissions(req, res, next) {
+    console.log(req.params.email);
     Submission.find({'user_email': req.params.email}, (err, submissions) => {
         if (err){
             res.status(500).send(err);   
@@ -63,6 +63,89 @@ export function getLatestSubmission(req, res, next) {
     });   
 }
 
+ /**
+ * Gets the latest submissions of all users in a specific assignment.
+ * @param req : User's request (should contain course_num, assignment_num as a parameter)
+ * @param res : The response back to the caller.
+ * Sends the latest submissions of all the users in a perticular assignment back as the response (a list of json object)
+ */
+
+export function getAllLatestSubmissions(req, res, next) {
+    
+    Course.findOne({'course_num': req.params.course_num}, (err, course) => {
+        if (err){
+            res.status(500).send(err);
+        } else {
+            course.assignments.forEach((assignment) => {
+                if(assignment.assignment_num == req.params.assignment_num){
+                    var calls = [];
+                    assignment.user_submissions.forEach((sub) => {
+                        calls.push(function(callback) {
+                            var temp = Submission.findOne({
+                                version: sub.submissions, 
+                                user_email: sub.email, 
+                                assignment_num: req.params.assignment_num,
+                                course_num: req.params.course_num
+                            }, (err, submissionObj) => {
+                                callback(null, submissionObj);
+                            });
+                        });
+                    });
+
+                    async.parallel(calls, function(err, result) {
+                        if (err) 
+                            return console.log(err);
+                        res.status(200).send(result);
+                    });
+                }
+            });
+        } 
+    });
+}
+
+ /**
+ * Gets the latest submissions of all users in all assignments.
+ * @param req : User's request (should contain course_num, assignment_num as a parameter)
+ * @param res : The response back to the caller.
+ * Sends the latest submissions of all the users in all the assignments back as the response (a list of json object)
+ */
+
+export function getAllLatestSubmissionsInAssignments(req, res, next) {
+    console.log("Got here!")
+    Course.findOne({'course_num': req.params.course_num}, (err, course) => {
+        if (err){
+            res.status(500).send(err);
+        } else {
+            course.assignments.forEach((assignment) => {
+                console.log(assignment);
+                console.log("====================================");
+                var calls = [];
+                assignment.user_submissions.forEach((sub) => {
+                    console.log(sub);
+                    calls.push(function(callback) {
+                        var temp = Submission.findOne({
+                            version: sub.submissions, 
+                            user_email: sub.email, 
+                            assignment_num: assignment.assignment_num,
+                            course_num: req.params.course_num
+                        }, (err, submissionObj) => {
+                            console.log(submissionObj);
+                            console.log("-----------------------------------------");
+                            callback(err, submissionObj);
+                        });
+                    });
+                });
+
+                async.parallel(calls, function(err, result) {
+                    if (err) 
+                        return console.log(err);
+                    res.status(200).send(result);
+                });
+            });
+        } 
+    });
+}
+
 /**
  * Gets all the submissions of a specific assignment.
  * @param req : User's request (should contain course_num and assignment_num as a parameter)
@@ -70,7 +153,8 @@ export function getLatestSubmission(req, res, next) {
  * Sends a list of all the submissions in a specific assignment back as the response (a list of json objects)
  */
 
-export function getAllSubmissions(req, res, next) {
+export function getAllSubmissions(req, res, next){
+    console.log("Got here!")
     Submission.find({"course_num":req.params.course_num}, (err, submissions) => {
         var submissionList = []
         submissions.forEach((submission) => {
@@ -79,7 +163,7 @@ export function getAllSubmissions(req, res, next) {
             }
         });
         res.status(200).send(submissionList);
-    }); 
+    });
 }
 
 /**
