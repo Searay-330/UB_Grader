21a13e15defd4fbd22e6bc76b6410eaa22e510de
Dhruv Kumar <dkumar2@buffalo.edu>
2017-10-23 02:19:04 -0400

    removed redundant file

--- PreferenceLists.py
+++
@@ -1,5 +0,0 @@
-class PreferenceLists:
-
-    def __init__(self, women_preferences, men_preferences):
-        self.women_preferences = women_preferences
-        self.men_preferences = men_preferences
