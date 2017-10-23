152f4c24be91ead7bf5c3dae278fb9d5057c6911
David Schuster <searay330@imageknight.com>
2017-10-23 03:28:20 -0400

    maybe fixed

--- client/modules/Assignment/AssignmentReducer.js
+++ client/modules/Assignment/AssignmentReducer.js
@@ -13,14 +13,16 @@ const AssignmentReducer = (state = initialState, action) => {
       var score;
       var feedback;
       var len = action.submission.length;
+
       if (action.submission[len-1] == undefined) {
         score = "No score recorded";
         feedback = "No submissions";
       } else {
         var total = 0;
-        score = action.submission[len-1].scores.forEach(function(element) {
+        console.log(action.submission[len-1].scores);
+        action.submission[len-1].scores.forEach(function(element) {
           console.log(element.score)
-          total = element.score;
+          total += element.score;
         });
         feedback = action.submission[len-1].feedback;
         score = total;
