25543f3a70f35f6b77d0a77adede0d6b21e10f90
Dhruv Kumar <dkumar2@buffalo.edu>
2017-10-29 17:53:43 -0400

    cleaned up code for other functions in SubmissionController and added async/await in all

--- server/controllers/SubmissionController.js
+++ server/controllers/SubmissionController.js
@@ -6,19 +6,6 @@ import * as AuthCheck from '../util/authentication'
 
 var async = require('async');
 
-function getCourse(num){
-    Course.findOne({'course_num': num}, (err, course) => {
-        if (err){
-            res.status(500).send(err);
-        }
-        if(course){
-            return course
-        } else {
-            return {}
-        }
-    });
-}
-
 /**
  * Gets all the user submissions of a specific assignment.
  * @param req : User's request (should contain course_num, assignment_num, email as a parameter)
@@ -50,7 +37,7 @@ export async function getUserSubmissions(req, res) {
  * Sends the latest submissions of a user in a perticular assignment back as the response (a list of json object)
  */
 
-export async function getLatestSubmission(req, res, next) {
+export async function getLatestSubmission(req, res) {
     try {
         let submissions = await Submission.findOne({
             "user_email": req.params.email, 
@@ -74,43 +61,37 @@ export async function getLatestSubmission(req, res, next) {
  * Sends the latest submissions of all the users in a perticular assignment back as the response (a list of json object)
  */
 
-export function getAllLatestSubmissions(req, res, next) {
-    
-    Course.findOne({'course_num': req.params.course_num}, (err, course) => {
-        if (err){
-            res.status(500).send(err);
-        } else {
-            course.assignments.forEach((assignment) => {
-                if(assignment.assignment_num == req.params.assignment_num){
-                    var submissionFound = false;
-                    var submissionList = [];
-                    assignment.user_submissions.forEach((sub) => {
-                        submissionFound = true;
-                        submissionList.push(function(callback) {
-                            var temp = Submission.findOne({
-                                version: sub.submissions, 
-                                user_email: sub.email, 
-                                assignment_num: req.params.assignment_num,
-                                course_num: req.params.course_num
-                            }, (err, submissionObj) => {
-                                callback(null, submissionObj);
-                            });
-                        });
-                    });
+export async function getAllLatestSubmissions(req, res) {
+    try {
+        let course = await Course.findOne({'course_num': req.params.course_num});
+        let assignment = course.assignments.id(req.params.assignment_num);
 
-                    async.parallel(submissionList, function(err, result) {
-                        if (err) 
-                            return res.status(500).send(err);
-                        if(submissionFound){
-                            res.status(200).send(result);
-                        } else {
-                            res.status(404).send({Status: 404, Message: "No submissions at the moment"});
-                        }
-                    });
-                }
+        var submissionList = [];
+        assignment.user_submissions.forEach((sub) => {
+            submissionList.push(function(callback) {
+                var temp = Submission.findOne({
+                    version: sub.submissions, 
+                    user_email: sub.email, 
+                    assignment_num: req.params.assignment_num,
+                    course_num: req.params.course_num
+                }, (err, submissionObj) => {
+                    callback(null, submissionObj);
+                });
             });
-        } 
-    });
+        });
+
+        async.parallel(submissionList, function(err, result) {
+            if (err) return res.status(500).send(err);
+            if(submissionList.length){
+                res.status(200).send(result);
+            } else {
+                res.status(404).send({Status: 404, Message: "No submissions at the moment"});
+            }
+        }); 
+
+    } catch(err){
+        res.status(500).send(err);
+    }
 }
 
  /**
@@ -120,60 +101,14 @@ export function getAllLatestSubmissions(req, res, next) {
  * Sends the latest submissions of a user in all assignments back as the response (a list of json object)
  */
 
-export function getLatestSubmissionsInAssignments(req, res, next) {
-    Course.findOne({'course_num': req.params.course_num}, (err, course) => {
-        if (err){
-            res.status(500).send(err);
-        } else {
-            var submissionFound = false;
-            var submissionList = [];
-            course.assignments.forEach((assignment) => {
-                assignment.user_submissions.forEach((sub) => {
-                    if(sub.email == req.params.email){
-                        submissionFound = true;
-                        submissionList.push(function(callback) {
-                            var temp = Submission.findOne({
-                                version: sub.submissions, 
-                                user_email: sub.email, 
-                                assignment_num: assignment.assignment_num,
-                                course_num: req.params.course_num
-                            }, (err, submissionObj) => {
-                                callback(err, submissionObj);
-                            });
-                        });
-                    }
-                });
-            });
-            async.parallel(submissionList, function(err, result) {
-                if (err) 
-                    return res.status(500).send(err);
-                if(submissionFound){
-                    res.status(200).send(result);
-                } else {
-                    res.status(404).send({Status: 404, Message: "No submissions from this user at the moment"});
-                }
-            });
-        } 
-    });
-}
+export async function getLatestSubmissionsInAssignments(req, res) {
+    try {
+        let course = await Course.findOne({'course_num': req.params.course_num});
 
- /**
- * Gets the latest submissions of all users in all assignments.
- * @param req : User's request (should contain course_num, assignment_num as a parameter)
- * @param res : The response back to the caller.
- * Sends the latest submissions of all the users in all the assignments back as the response (a list of json object)
- */
-
-export function getAllLatestSubmissionsInAssignments(req, res, next) {
-    Course.findOne({'course_num': req.params.course_num}, (err, course) => {
-        if (err){
-            res.status(500).send(err);
-        } else {
-            var submissionList = [];
-            var submissionFound = false;
-            course.assignments.forEach((assignment) => {
-                assignment.user_submissions.forEach((sub) => {
-                    submissionFound = true;
+        var submissionList = [];
+        course.assignments.forEach((assignment) => {
+            assignment.user_submissions.forEach((sub) => {
+                if(sub.email == req.params.email){
                     submissionList.push(function(callback) {
                         var temp = Submission.findOne({
                             version: sub.submissions, 
@@ -184,19 +119,63 @@ export function getAllLatestSubmissionsInAssignments(req, res, next) {
                             callback(err, submissionObj);
                         });
                     });
-                });
-            });
-            async.parallel(submissionList, function(err, result) {
-                if (err) 
-                    return res.status(500).send(err);
-                if(submissionFound){
-                    res.status(200).send(result);
-                } else {
-                    res.status(404).send({Status: 404, Message: "No submissions at the moment"});
                 }
             });
-        } 
-    });
+        });
+
+        async.parallel(submissionList, function(err, result) {
+            if (err) return res.status(500).send(err);
+            if(submissionList.length){
+                res.status(200).send(result);
+            } else {
+                res.status(404).send({Status: 404, Message: "No submissions from this user at the moment"});
+            }
+        });
+        
+    } catch(err){
+        res.status(500).send(err);
+    }
+}
+
+ /**
+ * Gets the latest submissions of all users in all assignments.
+ * @param req : User's request (should contain course_num, assignment_num as a parameter)
+ * @param res : The response back to the caller.
+ * Sends the latest submissions of all the users in all the assignments back as the response (a list of json object)
+ */
+
+export async function getAllLatestSubmissionsInAssignments(req, res) {
+    try{
+        let course = await Course.findOne({'course_num': req.params.course_num});
+
+        var submissionList = [];
+        course.assignments.forEach((assignment) => {
+            assignment.user_submissions.forEach((sub) => {
+                submissionList.push(function(callback) {
+                    var temp = Submission.findOne({
+                        version: sub.submissions, 
+                        user_email: sub.email, 
+                        assignment_num: assignment.assignment_num,
+                        course_num: req.params.course_num
+                    }, (err, submissionObj) => {
+                        callback(err, submissionObj);
+                    });
+                });
+            });
+        });
+        async.parallel(submissionList, function(err, result) {
+            if (err) 
+                return res.status(500).send(err);
+            if(submissionList.length){
+                res.status(200).send(result);
+            } else {
+                res.status(404).send({Status: 404, Message: "No submissions at the moment"});
+            }
+        });
+
+    } catch(err){
+        res.status(500).send(err);
+    }
 }
 
 /**
@@ -206,22 +185,19 @@ export function getAllLatestSubmissionsInAssignments(req, res, next) {
  * Sends a list of all the submissions in a specific assignment back as the response (a list of json objects)
  */
 
-export function getAllSubmissions(req, res, next){
-    Submission.find({"course_num":req.params.course_num}, (err, submissions) => {
-        var submissionList = []
-        var submissionFound = false;
-        submissions.forEach((submission) => {
-            if(submission.assignment_num == req.params.assignment_num){
-                submissionFound = true;
-                submissionList.push(submission);
-            }
+export async function getAllSubmissions(req, res){
+    try{
+        let submissions = await Submission.find({
+            'course_num': req.params.course_num,
         });
-        if(submissionFound){
-            res.status(200).send(submissionList);
+        if(submissions.length){
+            res.status(200).send(submissions);
         } else {
-            res.status(404).send({Status: 404, Message: "No submissions at the moment"});
+            res.status(404).send({Status: 404, Message: "No submissions from this user at the moment"});
         }
-    });
+    } catch(err) {
+        res.status(500).send(err);
+    }
 }
 
 /**
@@ -231,7 +207,7 @@ export function getAllSubmissions(req, res, next){
  * Sends back a JSON object of the created submission.
  */
 
-export async function createSubmission(req, res, next) {
+export async function createSubmission(req, res) {
     if (!req.files){
         res.status(400).send({Status: 400, Message: 'Sorry, you must submit exactly one file'});
     }
@@ -239,7 +215,7 @@ export async function createSubmission(req, res, next) {
         try{
             let user = await User.findOne({"email": req.user.email});
             let course = await Course.findOne({'course_num': req.params.course_num});
-            let  assignment = course.assignments.id(req.params.assignment_num);
+            let assignment = course.assignments.id(req.params.assignment_num);
             assignment.user_submissions.forEach((sub) => {
                 if(sub.email == user.email){
                     var submission = new Submission();
@@ -281,7 +257,7 @@ export async function createSubmission(req, res, next) {
  * Sends back a JSON object of the updated submission.
  */
 
-export function updateSubmission(req, res, next) {
+export function updateSubmission(req, res) {
     Course.findOne({'course_num': req.params.course_num}, (err, course) => {
         if (err){
             res.status(500).send(err);
@@ -319,7 +295,7 @@ export function updateSubmission(req, res, next) {
  * Sends back a JSON object of the deleted submission.
  */
 
-export function deleteSubmission(req, res, next) {
+export function deleteSubmission(req, res) {
     Submission.findByIdAndRemove(req.params.submission_id, (err, submissionObj) =>{
 //        if (err) res.status(500).send(err);
         res.status(200).send(submissionObj);
