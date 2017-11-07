722e12bbde059257faa96ce085689e9f2130fcb7
David Schuster <searay330@imageknight.com>
2017-11-06 21:08:39 -0500

    working on the nav bar might not be pretty but i will be functional

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -17,20 +17,22 @@ import styles from './Header.css';
 export function Header(props) {
   var displayMenu = props.menuItems && props.user != "";
   displayMenu = (displayMenu == undefined)? false : displayMenu;
-  var appBar = <AppBar showMenuIconButton={false} onTitleTouchTap={()=>{window.location="/"}} title={<span className={styles.point}>Autograder 3.0</span>} />;
+  var appBar = <AppBar showMenuIconButton={false} onTitleTouchTap={()=>{window.location="/"}} title={<span className={[styles.point, styles.unselectable].join(' ')}>Autograder 3.0</span>} />;
   var menu = null;
+  var nav = null;
   if (props.user != "") {
     var icon = <FlatButton label={props.user} labelPosition="before" icon={<ActionExitToApp />} href="/api/logout" />
-    appBar = <AppBar showMenuIconButton={displayMenu} onLeftIconButtonTouchTap={() => {props.drawerFunction(true);}} onTitleTouchTap={()=>{window.location="/"}} title={<span className={styles.point}>Autograder 3.0</span>} iconElementRight={icon} />;
+    appBar = <AppBar showMenuIconButton={displayMenu} onLeftIconButtonTouchTap={() => {props.drawerFunction(true);}} title={<span onClick={()=>{window.location="/"}} className={[styles.point, styles.unselectable].join(' ')}>Autograder 3.0</span>} iconElementRight={icon} />;
+    nav = <div className={styles.nav}><div onClick={()=>{console.log("click")}}className={[styles.unselectable, styles.travelLink,styles.point].join(" ")}>Courses</div></div>;
     if(displayMenu)
     {
       menu = <Drawer docked={false} width={200} open={props.drawerVar} onRequestChange={(open)=>{props.drawerFunction(open)}}>{props.menuItems}</Drawer>;
     }
-
   }
   return (
     <div>
         {appBar}
+        {nav}
         {menu}
     </div>
   );
